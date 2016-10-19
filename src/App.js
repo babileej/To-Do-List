636e33f491f3769321e3dba116d27b4a22cf2903
babileej <bailee@pdx.edu>
2016-10-18 23:34:08 -0700

    Added Checkbox

--- src/App.js
+++ src/App.js
@@ -5,23 +5,49 @@ export default class App extends Component {
     return (
       // Add your component markup and other subcomponent references here.
         <div>
-            <h1>Your To Do List</h1>
+            <h2>To Do List</h2>
             <UserInput />
+            <List />
         </div>
     );
   }
 }
 
 class UserInput extends Component{
-  render(){
-    return(
-        //Allows the user to input their list
-        <form>
-            <div className="form-group">
-              <label>New Item:</label>
-              <input type="text" className="NewToDo"></input>
+    render(){
+        return(
+            //Allows the user to input their list
+            <form action="">
+             <div className="form-group">
+               <label>Please enter an item on your list: </label>
+               <br><input type="text" className="NewToDo"></input></br>
+             </div>
+            </form>
+        )
+    }
+}
+
+class List extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: props};
+    }
+    render(){
+        return(
+            <ToDoItem></ToDoItem>
+        );
+    }
+}
+
+class ToDoItem extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="checkbox">
+                <label><input type="checkbox" value="">{this.props.children}</input></label>
             </div>
-        </form>
-    )
-  }
+        );
+    }
 }
